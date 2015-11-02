5813616cd81a0818d761590a7967b96a4ab924c6
Li Chen wei <enoch_lee@hiiir.com>
2015-11-02 20:22:19 +0800

    create router.rb root router and create bmi controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+	get "/", to:"bmi#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
